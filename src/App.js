cbb198de468defe5fe124a5b11a3220f58c0ac6a
zohaib ali shah <johndoe@example.com>
2022-06-21 21:50:01 +0500

    inital

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import { Fragment } from "react";
+import Header from "./Header";
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        console.log(res);
+      });
+
+    // console.log("hello mount")
+  }, []);
+
+  const [name, setName] = useState("zohaib");
+  const [email, setEmail] = useState("");
+
+  function login() {
+    const data = {
+      name: name,
+      email: email,
+    };
+    console.log("server", data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header name={name} />
+      <h1>Login</h1>
+      <label>Name</label>
+      <input name="name" onChange={(e) => setName(e.target.value)} />
+      <br />
+      <label>Email</label>
+      <input name="email" onChange={(e) => setEmail(e.target.value)} />
+      <br />
+      <button onClick={login}>Login </button>
+      <p>Name :{name}</p>
+      <p>Email :{email}</p>
+    </Fragment>
   );
 }
 
